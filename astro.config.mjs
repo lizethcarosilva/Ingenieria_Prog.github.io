aa226027dfb25a00979e917fa1228ebe098bc383
Lizeth_caro <lizethandreacarosilva@gmail.com>
2025-10-09 12:43:12 -0500

    fix: update base URL handling in layout and adjust image paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://lizethcarosilva.github.io',
-  base: '/',
+  //base: '/',
   integrations: [tailwind()]
 });
